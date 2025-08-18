423f324d37fdaa3f2c09704980cbaa5636c3a733
BlackTechOF <bllacktechoficial@gmail.com>
2025-08-18 18:01:29 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-const map = L.map('map').setView([-46.51457426020385, -23.5806818462156085], 11);
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
+      const map = L.map('map').setView([latitude, longitude], 12);
 
-// Carregar os ecopontos
-fetch('ecopontos.geojson') // ajuste o caminho se estiver em outra pasta
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      onEachFeature: (feature, layer) => {
-        if (feature.properties && feature.properties.nome) {
-          layer.bindPopup(
-            `<b>${feature.properties.nome}</b><br>${feature.properties.endereco || ''}`
-          );
-        }
-      }
-    }).addTo(map);
-  })
-  .catch(err => console.error("Erro ao carregar GeoJSON:", err));
+      console.log(`Latitude: ${latitude}`)
+      console.log(`Longitude: ${longitude}`)
 
-// Ativar localização do usuário
-map.locate({ setView: true, maxZoom: 16 });
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+      }).addTo(map);
 
-// Evento de localização encontrada
-map.on("locationfound", function(e) {
-  L.marker(e.latlng).addTo(map)
-    .bindPopup("📍 Você está aqui!").openPopup();
-});
+      L.marker([latitude, longitude]).addTo(map)
+        .bindPopup("📍 Você está aqui!").openPopup();
 
-// Caso não consiga acessar a localização
-map.on("locationerror", function() {
-  alert("Não foi possível acessar sua localização.");
-});
+      fetch('ecopontos.geojson')
+        .then(response => response.json())
+        .then(data => {
+          L.geoJSON(data, {
+            onEachFeature: (feature, layer) => {
+              if (feature.properties && feature.properties.nome) {
+                layer.bindPopup(
+                  `<b>${feature.properties.nome}</b><br>${feature.properties.endereco || ''}`
+                );
+              }
+            }
+          }).addTo(map);
+        })
+        .catch(err => console.error("Erro ao carregar GeoJSON:", err));
+    },
+    () => {
+      alert("Não foi possível acessar sua localização.");
+    }
+  );
+} else {
+  alert("Geolocalização não é suportada no seu navegador.");
+}
