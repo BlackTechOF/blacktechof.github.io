513355f760c2d8023a205597940519fb60815931
BlackTechOF <bllacktechoficial@gmail.com>
2025-09-25 13:41:09 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,19 +1,17 @@
 const inputFontSize = document.getElementById('inputFontSize');
 const demoFonte = document.getElementById('demoFonte');
 
+inputFontSize.addEventListener('input', function(){
+    demoFonte.style.fontSize = inputFontSize.value + 'px'
+});
+
 inputFontSize.addEventListener('keydown', function(e){
   if (e.key === 'Enter') {
     console.log(inputFontSize.value)
+ localStorage.setItem("fontSize", inputFontSize.value);
+ localStorage.getItem('fontSize');
   }
 })
-console.log(inputFontSize.value)
 
-inputFontSize.addEventListener('input', function(){
-    demoFonte.style.fontSize = inputFontSize.value + 'px'
-})
-
-const setarFontSize = localStorage.setItem("fontSize", inputFontSize.value);
-
-const fonteSalva = localStorage.getItem('fontSize');
 
 
