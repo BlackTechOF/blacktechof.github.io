a624eaf07e6b2799cc7b1e08d8ff9e3d04b80163
BlackTechOF <bllacktechoficial@gmail.com>
2025-09-25 21:08:15 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,36 @@
 const inputFontSize = document.getElementById('inputFontSize');
 const demoFonte = document.getElementById('demoFonte');
+const btnConfirmFont = document.getElementById('confirmFont');
+const selectFont = document.getElementById('selectFont');
 
-inputFontSize.addEventListener('input', function(){
-    demoFonte.style.fontSize = inputFontSize.value + 'px'
+// Pega os valores salvos no localStorage ao carregar a página
+const valorFontSize = localStorage.getItem('fontSize');
+const valorFontFamily = localStorage.getItem('fontFamily');
+
+// Adiciona um event listener para atualizar o estilo e o localStorage quando o valor da fonte mudar
+selectFont.addEventListener('change', function(){
+    demoFonte.style.fontFamily = selectFont.value;
 });
 
+// Adiciona um event listener para atualizar o estilo quando o valor do tamanho da fonte mudar
 inputFontSize.addEventListener('input', function(){
-    console.log(inputFontSize.value)
- localStorage.setItem("fontSize", inputFontSize.value);
- localStorage.getItem('fontSize');
-})
+    demoFonte.style.fontSize = inputFontSize.value + 'px';
+});
 
+// Adiciona um event listener para salvar no localStorage quando o botão for clicado
+btnConfirmFont.addEventListener('click', function(){
+    console.log(inputFontSize.value);
+    localStorage.setItem("fontSize", inputFontSize.value);
+    localStorage.setItem('fontFamily', selectFont.value);
+});
 
+// Define os valores iniciais dos inputs e da demoFonte com os dados do localStorage (se existirem)
+if (valorFontSize) {
+    inputFontSize.value = valorFontSize;
+    demoFonte.style.fontSize = valorFontSize + 'px'; // Aplica o estilo na demoFonte
+}
 
-
+if (valorFontFamily) {
+    selectFont.value = valorFontFamily;
+    demoFonte.style.fontFamily = valorFontFamily; // Aplica o estilo na demoFonte
+}
